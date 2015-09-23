b9c137c1b89f003911fd6cb9bcef81eb013e2ed5
Ken Franken <ken.franken@syncbak.com>
2015-09-23 15:14:03 -0500

    more tests and an example

--- recurring_test.go
+++ recurring_test.go
@@ -36,3 +36,36 @@ func TestDeadline(t *testing.T) {
 		t.Errorf("Bad duration, expected 24 hours, got %f", d.Hours())
 	}
 }
+
+func ExampleRecurring() {
+	// set a recurring tick at 01:02:03 UTC for the next 3 days
+	r := New(1, 2, 3, 0)
+	defer r.Stop()
+	for i := 0; i < 3; i++ {
+		<-r.C
+		// time will now be 01:02:03 UTC
+	}
+}
+
+func TestRecurring(t *testing.T) {
+	now := time.Now().UTC()
+	r := New(now.Hour(), now.Minute(), now.Second(), now.Nanosecond()+5e6)
+	defer r.Stop()
+	// we don't want to wait 24 hours for the test to complete, so we cheat and
+	// change the deadline. This won't affect the first event, but will change
+	// the second one.
+	r.nsec += 5e6
+	<-r.C
+	elapsed := time.Since(now)
+	if elapsed.Nanoseconds()/1e6 != 5 {
+		t.Fatalf("Bad duration, expected 5ms, got %d", elapsed.Nanoseconds()/1e6)
+	}
+	now = time.Now().UTC()
+	<-r.C
+	elapsed = time.Since(now)
+	msec := elapsed.Nanoseconds() / 1e6
+	// there is a little slop, so look for a range
+	if msec < 4 || msec > 6 {
+		t.Errorf("Bad duration, expected 5ms, got %d", msec)
+	}
+}
